6d535d5c31abde506a34fd3a67fe00ff7a7dbb9e
Mehmet Beydogan <mbbrain@gmail.com>
2013-10-19 04:05:31 +0300

    fonts folder is added to asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Confman
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
   end
 end
