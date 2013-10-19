eed225a04ece5f1e3c83b7f5f8075ece5f8b3d9b
Zafer CAKMAK <mzcakmak@gmail.com>
2013-10-19 04:40:57 +0300

    adds organization relation to conference

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -1,8 +1,9 @@
 class Conference < ActiveRecord::Base
-  attr_accessible :description, :email, :facebook, :name, :phone, :summary, :twitter, :website, :address_attributes
+  attr_accessible :description, :email, :facebook, :name, :phone, :summary, :twitter, :website, :address_attributes, :organization_ids
 
   has_one :address
   has_and_belongs_to_many :organizations
 
   accepts_nested_attributes_for :address
+  accepts_nested_attributes_for :organizations
 end
