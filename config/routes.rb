0999ba7c2a4141e2cb699cc00f2fd3dd4d051d32
Mehmet Beydogan <mbbrain@gmail.com>
2013-10-19 03:21:41 +0300

    home controller is created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Confman::Application.routes.draw do
+  get "home/index"
+
   devise_for :users
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ Confman::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
