7c8ec073d87fe54e0f9ef122364ac79e7ff4115a
Zafer CAKMAK <mzcakmak@gmail.com>
2013-10-20 00:37:47 +0300

    adds state machine to appeal

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131019191509) do
+ActiveRecord::Schema.define(:version => 20131019210908) do
 
   create_table "addresses", :force => true do |t|
     t.text     "info"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20131019191509) do
     t.integer  "topic_id"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+    t.string   "state"
   end
 
   create_table "comments", :force => true do |t|
