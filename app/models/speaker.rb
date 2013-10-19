1d3c73bc590dc2d168ff2975037a6416e9479e5a
Mehmet Beydogan <mbbrain@gmail.com>
2013-10-19 20:51:53 +0300

    avatar_url is added to speaker

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -2,4 +2,9 @@ class Speaker < ActiveRecord::Base
   attr_accessible :email, :facebook, :name, :phone, :twitter, :bio, :github
 
   has_and_belongs_to_many :topics
+
+  def avatar_url
+    gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=150"
+  end
 end
