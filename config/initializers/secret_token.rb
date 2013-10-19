0ab99e6227390ae130e0f5d43cba0155d8e87aa9
Mehmet Beydogan <mbbrain@gmail.com>
2013-10-19 03:03:29 +0300

    rails new confman

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Confman::Application.config.secret_token = '5fb8ac820f5d51577cf72ce82bfebc8b243e3eaa6ba4ac5e197a1562a86d0657e2a89e2a9faf2b56b9da5a0bf8ee9795a15a8886c445299fbaaa13ae637c927b'
