5b375036aad0b30d9124a6d6e94ecff73d2b3c7f
Mehmet Beydogan <mbbrain@gmail.com>
2013-10-20 17:52:23 +0300

    upcoming events is added to homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,11 @@ class HomeController < ApplicationController
   layout "home_layout", :only => [:index]
 
   def index
+    @days = Day.joins(:conferences).where('date > ?', Date.today).order("date DESC").group("conferences.id")
+    @conferences = Conference.includes(:days).order('days.date ').where('days.date > ?', Date.today).group("conferences.id").limit(5)
+
   end
 
   def dashboard
-
   end
 end
--- app/models/day.rb
+++ app/models/day.rb
@@ -2,4 +2,6 @@ class Day < ActiveRecord::Base
   attr_accessible :date
 
   has_many :slots
+  has_and_belongs_to_many :conferences
+
 end
