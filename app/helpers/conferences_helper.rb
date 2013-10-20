d0ab536c397a3cb11490afa5de756a593db2f839
Destan Sarpkaya <destan@dorukdestan.com>
2013-10-20 11:40:21 +0000

    show slots like a boss

--- app/helpers/conferences_helper.rb
+++ app/helpers/conferences_helper.rb
@@ -1,2 +1,33 @@
 module ConferencesHelper
+
+  # days = {
+  #   day_id: {
+  #     room_id: {
+  #       slot_id: {
+  #         slot
+  #       }
+  #     }
+  #   }
+  # }
+  def self.slots_data(conference)
+    days = {}
+    rooms = conference.rooms
+
+    conference.days.each do |day| 
+      d = {}
+
+      rooms.each do |room|
+        r = {}
+        Slot.where(conference_id: conference.id, day_id: day[:id], room_id: room[:id]).each do |slot|
+          r[slot.id] = slot
+        end
+        d[room[:id]] = r
+      end
+
+      days[day[:id]] = d
+    end
+
+    return days.to_json
+  end
+
 end
