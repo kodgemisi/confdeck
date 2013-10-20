9ef03eb37c4f341a28ba8d9dcf5295bdb913b244
Zafer CAKMAK <mzcakmak@gmail.com>
2013-10-20 15:43:17 +0300

    makes rooms nested resource of conference

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,9 @@
 class RoomsController < ApplicationController
   before_filter :authenticate_user!
 
+  before_filter :set_conference
+  before_filter :set_room, only: [:show, :edit, :update, :destroy]
+
   # GET /rooms
   # GET /rooms.json
   def index
@@ -15,8 +18,6 @@ class RoomsController < ApplicationController
   # GET /rooms/1
   # GET /rooms/1.json
   def show
-    @room = Room.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @room }
@@ -36,17 +37,17 @@ class RoomsController < ApplicationController
 
   # GET /rooms/1/edit
   def edit
-    @room = Room.find(params[:id])
   end
 
   # POST /rooms
   # POST /rooms.json
   def create
     @room = Room.new(params[:room])
+    @room.conference_id = @conference.id
 
     respond_to do |format|
       if @room.save
-        format.html { redirect_to @room, notice: 'Room was successfully created.' }
+        format.html { redirect_to schedule_conference_path(@conference), notice: 'Room was successfully created.' }
         format.json { render json: @room, status: :created, location: @room }
       else
         format.html { render action: "new" }
@@ -58,11 +59,10 @@ class RoomsController < ApplicationController
   # PUT /rooms/1
   # PUT /rooms/1.json
   def update
-    @room = Room.find(params[:id])
 
     respond_to do |format|
       if @room.update_attributes(params[:room])
-        format.html { redirect_to @room, notice: 'Room was successfully updated.' }
+        format.html { redirect_to [@conference, @room], notice: 'Room was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -74,12 +74,21 @@ class RoomsController < ApplicationController
   # DELETE /rooms/1
   # DELETE /rooms/1.json
   def destroy
-    @room = Room.find(params[:id])
     @room.destroy
 
     respond_to do |format|
-      format.html { redirect_to rooms_url }
+      format.html { redirect_to conference_rooms_url(@conference) }
       format.json { head :no_content }
     end
+  end
+
+  private
+
+  def set_conference
+    @conference = Conference.find(params[:conference_id])
+  end
+
+  def set_room
+    @room = @conference.rooms.find(params[:id])
   end
 end
