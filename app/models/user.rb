ae4b6082804a5ce9d34b75fb110aa9f79770bdb5
Mehmet Beydogan <mbbrain@gmail.com>
2013-11-21 20:07:07 +0200

    avatar_url method added to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,9 @@ class User < ActiveRecord::Base
   has_many :conferences, through: :organizations, uniq: true
 
   acts_as_voter
+
+  def avatar_url(size=150)
+    gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+  end
 end
