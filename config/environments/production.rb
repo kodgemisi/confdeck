c5b1ec1c98f32355e3903cea9739cc168fb05e8a
Zafer CAKMAK <mzcakmak@gmail.com>
2014-02-09 11:25:53 +0200

    fixes typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Confman::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( bootstrap.js bootstrap-custom.css font-awesome.min.css home.css timeline.css home.js schedule.js conferences.js organization.js speakers.js appeals.js invitations.js rooms.js slots.js sponsors.js topics.js )
+  config.assets.precompile += %w( bootstrap.js bootstrap-custom.css font-awesome.min.css home.css timeline.css home.js schedule.js conferences.js organizations.js speakers.js appeals.js invitations.js rooms.js slots.js sponsors.js topics.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
