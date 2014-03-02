8b2a17890bf932fd1e6c3dc45f1eecc899935c99
Mehmet Beydogan <mbbrain@gmail.com>
2014-03-02 23:23:07 +0200

    adds relation with conference and fixes reserved word error on EmailTemplate

--- app/models/email_template.rb
+++ app/models/email_template.rb
@@ -1,3 +1,7 @@
 class EmailTemplate < ActiveRecord::Base
   attr_accessible :conference_id, :subject, :template, :type
+
+  belongs_to :conference
+
+  self.inheritance_column = :class_type #because type field is reserved for rails
 end
