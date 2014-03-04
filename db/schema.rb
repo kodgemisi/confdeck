7d13e541b970e052888b80d863453bc364927134
Mehmet Beydogan <mbbrain@gmail.com>
2014-03-04 17:39:09 +0200

    runs migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140304134358) do
+ActiveRecord::Schema.define(:version => 20140304152105) do
 
   create_table "addresses", :force => true do |t|
     t.text     "info"
