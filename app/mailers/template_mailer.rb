a7540deadf20bd4927fcbb0adfc800f89b96f43b
Mehmet Beydogan <mbbrain@gmail.com>
2014-03-04 15:52:32 +0200

    adds loading default template if conference template is missing on TemplateMailer module

--- app/mailers/template_mailer.rb
+++ app/mailers/template_mailer.rb
@@ -2,8 +2,15 @@ module TemplateMailer
 
   def prepare(conference, type, vars = {}, subject_vars = {})
     template = conference.email_templates.joins(:email_template_type).where("email_template_types.type_name" => type).first
-    body_liquid = Liquid::Template.parse(template.body)
-    subject_liquid = Liquid::Template.parse(template.subject)
+
+    if template.present? #if conference has custom email
+      body_liquid = Liquid::Template.parse(template.body)
+      subject_liquid = Liquid::Template.parse(template.subject)
+    else #load default from EmailTemplateType
+      template_type = EmailTemplateType.find_by type_name: type
+      body_liquid = Liquid::Template.parse(template_type.default_body)
+      subject_liquid = Liquid::Template.parse(template_type.default_subject)
+    end
 
     body = body_liquid.render(vars)
 
