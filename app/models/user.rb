0920d01ae8c39144007930926aba333b30e2d75a
Mehmet Beydogan <mbbrain@gmail.com>
2014-05-17 22:27:19 +0300

    adds display name metho to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,4 +30,8 @@ class User < ActiveRecord::Base
     gravatar_id = Digest::MD5::hexdigest(self.email).downcase
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
   end
+
+  def display_name
+    email
+  end
 end
