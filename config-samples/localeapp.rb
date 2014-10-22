c7fa623927b0b840db4ab97a7581635ced72c218
Mehmet Beydogan <mbbrain@gmail.com>
2014-10-22 19:59:43 +0300

    updates localeapp sample config

--- config-samples/localeapp.rb
+++ config-samples/localeapp.rb
@@ -4,4 +4,6 @@ require 'localeapp/rails'
 Localeapp.configure do |config|
     config.api_key = '' # add key when you get it, Localeapp complains if this is unset!
     # you'll also want to add other config on setup, see README.md
+    config.polling_environments = []
+
 end
