45554f7b0de99c0c7cb3febfe3d3e1c4b371fa0c
Zafer CAKMAK <zafer@kodgemisi.com>
2014-10-22 16:45:32 +0300

    adds localeapp sample config

---
+++ config-samples/localeapp.rb
@@ -0,0 +1,7 @@
+# config/initializers/localeapp.rb
+require 'localeapp/rails'
+
+Localeapp.configure do |config|
+    config.api_key = '' # add key when you get it, Localeapp complains if this is unset!
+    # you'll also want to add other config on setup, see README.md
+end
