4a5679cb4710f114c61d96f996fe17513f0e30a9
Mehmet Beydogan <mbbrain@gmail.com>
2014-10-24 22:09:14 +0300

    fixes missing translation bug

--- app/helpers/i18n_helper.rb
+++ app/helpers/i18n_helper.rb
@@ -2,6 +2,7 @@ module I18nHelper
   def translate(key, options={})
     super(key, options.merge(raise: true))
   rescue I18n::MissingTranslationData
+    key = key.to_s if key.is_a? Symbol
     key.split(".").last.humanize + "*"
   end
   alias :t :translate
