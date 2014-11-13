7ada57eb71189edb112a2b0897ba3deb9e53ad66
Mehmet Beydogan <mbbrain@gmail.com>
2014-11-13 20:40:50 +0200

    fixes default scope syntax on Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -17,8 +17,7 @@ class Comment < ActiveRecord::Base
 
   belongs_to :commentable, :polymorphic => true
 
-  default_scope :order => 'created_at ASC'
-
+  default_scope { order("created_at DESC") }
   # NOTE: install the acts_as_votable plugin if you
   # want user to vote on the quality of comments.
   #acts_as_voteable
