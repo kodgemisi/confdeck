f9e2de7ae6108bacc06e42fdf7d5bb4ff66ed7d8
Mehmet Beydogan <mbbrain@gmail.com>
2014-12-04 10:44:18 +0200

    fixes localeapp sending on each request

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -5,4 +5,6 @@ Localeapp.configure do |config|
     config.api_key = ENV["LOCALEAPP_KEY"]
     # you'll also want to add other config on setup, see README.md
     config.polling_environments = []
+    config.sending_environments = []
+
 end
