421cbe1e24dda877118dff90ca1b377f4c4c3d00
Mehmet Beydogan <m@mehmet.pw>
2014-12-05 18:07:23 +0200

    adds sponsors to conference wizard

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -63,6 +63,7 @@ class Conference < ActiveRecord::Base
 
 
 
+  accepts_nested_attributes_for :sponsors
   accepts_nested_attributes_for :address
   accepts_nested_attributes_for :organizations
   accepts_nested_attributes_for :days
