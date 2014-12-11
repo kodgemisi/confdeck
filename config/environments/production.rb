72ebf1ed4250c01019d76f06d2600033ced5cc65
Zafer CAKMAK <zafer@kodgemisi.com>
2014-12-11 15:08:46 +0200

    updates asset urls to work with production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,8 +16,7 @@ Confman::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -48,7 +47,7 @@ Confman::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( bootstrap.js bootstrap-custom.css font-awesome.min.css home.css timeline.css home.js schedule.js conferences.js jquery.js organizations.js speakers.js appeals.js invitations.js rooms.js slots.js sponsors.js topics.js conference_landing.css conference_landing.js )
+  config.assets.precompile += %w( bootstrap.js bootstrap-custom.css font-awesome.min.css home.css timeline.css home.js schedule.js conferences.js jquery.js organizations.js speakers.js appeals.js invitations.js rooms.js slots.js sponsors.js topics.js conference_landing.css conference_landing.js iconfont.css)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
