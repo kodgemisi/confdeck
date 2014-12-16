33af29ae6d9f4dc1d27ecb7dcc13a8553507e61b
Mehmet Beydogan <mbbrain@gmail.com>
2014-12-16 16:50:47 +0200

    adds capybara-webkit gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'capybara/rspec'
 require 'capybara/rails'
+Capybara.javascript_driver = :webkit
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
