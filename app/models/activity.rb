929966a6443bfaf637a21545d66138853bd2ec07
Mehmet Beydogan <mbbrain@gmail.com>
2015-01-09 16:50:37 +0200

    Removes unsued code from activity.rb

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,14 +3,4 @@ class Activity < ActiveRecord::Base
   belongs_to :conference
   belongs_to :user
 
-  after_create :set_conference
-
-
-  def set_conference
-    self.conference = subject.conference if self.conference.nil?
-  end
-
-  def target=(target)
-    self.conference = target
-  end
 end
