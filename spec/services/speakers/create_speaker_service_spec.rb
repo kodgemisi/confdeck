2286380c4c1ea64913615c623dd6cc4cade1800c
Mehmet Beydogan <mbbrain@gmail.com>
2015-01-20 10:46:22 +0200

    Fix create_speaker_service_spec

--- spec/services/speakers/create_speaker_service_spec.rb
+++ spec/services/speakers/create_speaker_service_spec.rb
@@ -5,7 +5,9 @@ describe Speakers::CreateSpeakerService  do
   it "returns speaker if it exists" do
     @speaker = Fabricate(:speaker)
     speaker_params = {
-        email: @speaker.user.email
+        user: {
+            email: @speaker.user.email
+        }
     }
 
     @response = Speakers::CreateSpeakerService.call(speaker_params)
@@ -15,7 +17,9 @@ describe Speakers::CreateSpeakerService  do
   it "creates speaker for user if it doesn't exist" do
     @user = Fabricate(:user)
     speaker_params = {
-        email: @user.email
+        user: {
+            email: @user.email
+        }
     }
 
     @response = Speakers::CreateSpeakerService.call(speaker_params)
@@ -26,12 +30,15 @@ describe Speakers::CreateSpeakerService  do
     mail = ActionMailer::Base.deliveries.count
     @speaker = Fabricate.build(:speaker)
     speaker_params = {
-        email: @speaker.user.email,
+        user: {
+            email: @speaker.user.email
+        },
         name: @speaker.name,
         phone: @speaker.phone
     }
 
     @response = Speakers::CreateSpeakerService.call(speaker_params)
+    @response.save
     expect(User.find_by(email: @speaker.user.email)).not_to be_nil
     expect( ActionMailer::Base.deliveries.count ).to eq(mail+1)
   end
