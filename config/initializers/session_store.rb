e3ed6ce1df2ae9bc7a83660a38796c8c541efca0
Mehmet Beydogan <mbbrain@gmail.com>
2015-01-21 15:30:00 +0200

    Fixes sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,13 @@
 # Be sure to restart your server when you modify this file.
 
-Confman::Application.config.session_store :cookie_store, key: '_confman_session_ex', :domain => '.confdeck.dev'
+if Rails.env == "production"
+  Confman::Application.config.session_store :cookie_store, key: '_confman_session_prod', :domain => '.confdeck.com'
+elsif Rails.env == "test"
+  Confman::Application.config.session_store :cookie_store, key: '_confman_session_test', :domain => '.example.com'
+else
+  Confman::Application.config.session_store :cookie_store, key: '_confman_session_dev', :domain => '.confdeck.dev'
+end
+
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
