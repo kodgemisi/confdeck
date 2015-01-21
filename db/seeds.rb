59c0c7564b8d8c0dd14b5561abbe3348cc62ceb6
Mehmet Beydogan <mbbrain@gmail.com>
2015-01-21 10:33:53 +0200

    Fixes seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ EmailTemplateType.create({
                              type_name: "accept_notification_email",
                              title: "Speech Accept Notification Email",
                              default_subject: "[{{conference.name}}] {{ topic.subject }} is accepted",
-                             default_body: "Your application is accepted for {{ speech.conference.name }} <br /><br/>
+                             default_body: "Your application is accepted for {{ conference.name }} <br /><br/>
                 <b>Speakers:</b> <br/>
                 <ul>
                  {% for speaker in speech.speakers %}
@@ -27,7 +27,7 @@ EmailTemplateType.create({
                              type_name: "reject_notification_email",
                              title: "Speech Reject Notification Email",
                              default_subject: "[{{conference.name}}] {{ topic.subject }} is rejected",
-                             default_body: "Your application is rejected for {{ speech.conference.name }} <br /><br/>
+                             default_body: "Your application is rejected for {{ conference.name }} <br /><br/>
             <b>Speakers:</b> <br/>
             <ul>
              {% for speaker in speech.speakers %}
@@ -48,7 +48,7 @@ EmailTemplateType.create({
                              type_name: "speaker_notification_email",
                              title: "Speaker Notification Email",
                              default_subject: "[{{conference.name}}] {{ topic.subject }} is received",
-                             default_body: "Your application is received for {{ speech.conference.name }} <br /><br/>
+                             default_body: "Your application is received for {{ conference.name }} <br /><br/>
             <b>Speakers:</b> <br/>
             <ul>
              {% for speaker in speech.speakers %}
@@ -69,7 +69,7 @@ EmailTemplateType.create({
                              type_name: "committee_notification_email",
                              title: "New Application Notification Email To Committee",
                              default_subject: "[{{conference.name}}] New application is received",
-                             default_body: "New application is received for {{ speech.conference.name }} <br /><br/>
+                             default_body: "New application is received for {{ conference.name }} <br /><br/>
             <b>Speakers:</b> <br/>
             <ul>
              {% for speaker in speech.speakers %}
