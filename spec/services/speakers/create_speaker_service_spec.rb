e9f11ea377751afeaab90a7d36c7f68aa5fb75a6
Mehmet Beydogan <mbbrain@gmail.com>
2015-01-27 16:39:17 +0200

    Fixes confecreate_speaker_service_spec

--- spec/services/speakers/create_speaker_service_spec.rb
+++ spec/services/speakers/create_speaker_service_spec.rb
@@ -4,40 +4,35 @@ describe Speakers::CreateSpeakerService  do
 
   it "returns speaker if it exists" do
     @speaker = Fabricate(:speaker)
-    speaker_params = {
-        user: {
+    speaker_form = SpeakerForm.new({
             email: @speaker.user.email
-        }
-    }
+    })
 
-    @response = Speakers::CreateSpeakerService.call(speaker_params)
+    @response = Speakers::CreateSpeakerService.instance.call(speaker_form)
     expect(@response.id).to eq @speaker.id
   end
 
   it "creates speaker for user if it doesn't exist" do
     @user = Fabricate(:user)
-    speaker_params = {
-        user: {
-            email: @user.email
-        }
-    }
+    speaker_form = SpeakerForm.new({
+         email: @user.email
+     })
 
-    @response = Speakers::CreateSpeakerService.call(speaker_params)
+    @response = Speakers::CreateSpeakerService.instance.call(speaker_form)
     expect(@response.user.id).to eq @user.id
   end
 
   it "creates user and speaker if it doesn't exist" do
     mail = ActionMailer::Base.deliveries.count
     @speaker = Fabricate.build(:speaker)
-    speaker_params = {
-        user: {
-            email: @speaker.user.email
-        },
-        name: @speaker.name,
-        phone: @speaker.phone
-    }
 
-    @response = Speakers::CreateSpeakerService.call(speaker_params)
+    speaker_form = SpeakerForm.new({
+      email: @speaker.user.email,
+      name: @speaker.name,
+      phone: @speaker.phone
+     })
+
+    @response = Speakers::CreateSpeakerService.instance.call(speaker_form)
     @response.save
     expect(User.find_by(email: @speaker.user.email)).not_to be_nil
     expect( ActionMailer::Base.deliveries.count ).to eq(mail+1)
