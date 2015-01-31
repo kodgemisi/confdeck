9453bb44435fddbd98f1b8700940b521a5e66388
Mehmet Beydogan <mbbrain@gmail.com>
2015-01-31 23:51:44 +0200

    Add feature specs for dashboard links for admin

--- spec/features/conferences/conference_dashboard_spec.rb
+++ spec/features/conferences/conference_dashboard_spec.rb
@@ -23,24 +23,59 @@ describe "Conference Dashboard", :type => :feature do
       @conference.conference_admins << @user
     end
 
-    it "can click and go to speech list" do
+    it "can click and go to Speeches" do
       visit(admin_conference_url(subdomain: @conference.slug))
       click_link "Speeches"
       expect(page).to have_content(I18n.t("titles.speeches.index", conference: @conference.name))
     end
 
-    it "can click and go to schedule" do
+    it "can click and go to Schedule" do
       visit(admin_conference_url(subdomain: @conference.slug))
       click_link "Schedule"
       expect(page).to have_content(I18n.t("titles.conferences.schedule", conference: @conference.name))
     end
 
-    it "can click and go to roles" do
+    it "can click and go to Roles" do
       visit(admin_conference_url(subdomain: @conference.slug))
       click_link "Roles"
       expect(page).to have_content(I18n.t("titles.roles.index", conference: @conference.name))
     end
 
+    it "can click and go to Email Templates" do
+      visit(admin_conference_url(subdomain: @conference.slug))
+      click_link "Email Templates"
+      expect(page).to have_content(I18n.t("titles.conferences.email_templates"))
+    end
+
+    it "can click and go to Sponsors" do
+      visit(admin_conference_url(subdomain: @conference.slug))
+      click_link "Sponsors"
+      expect(page).to have_content(I18n.t("titles.sponsors.index", conference: @conference.name))
+    end
+
+    it "can click and go to Speech Types" do
+      visit(admin_conference_url(subdomain: @conference.slug))
+      click_link "Speech Types"
+      expect(page).to have_content(I18n.t("titles.conferences.speech_types", conference: @conference.name))
+    end
+
+    it "can click and go to Basic Information" do
+      visit(admin_conference_url(subdomain: @conference.slug))
+      click_link "Basic Information"
+      expect(page).to have_content(I18n.t("titles.conferences.basic_information", conference: @conference.name))
+    end
+
+    it "can click and go to Address" do
+      visit(admin_conference_url(subdomain: @conference.slug))
+      click_link "Address"
+      expect(page).to have_content(I18n.t("titles.conferences.address", conference: @conference.name))
+    end
+
+    it "can click and go to Contact Information" do
+      visit(admin_conference_url(subdomain: @conference.slug))
+      click_link "Contact Information"
+      expect(page).to have_content(I18n.t("titles.conferences.contact_information", conference: @conference.name))
+    end
 
   end
 
