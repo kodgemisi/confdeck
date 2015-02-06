c04ded1b4ba14a490eb63fab9264592a3f4d2148
Mehmet Beydogan <mbbrain@gmail.com>
2015-02-06 15:59:42 +0200

    Fix email templates

--- app/assets/javascripts/email_templates.js
+++ app/assets/javascripts/email_templates.js
@@ -4,4 +4,17 @@
 $(document).ready(function(){
    initializeEditors();
 
+   $("#update-templates").click(function( event ) {
+       event.preventDefault();
+
+       //Set WYSIWYG editor contents to hidden fields before submit
+       var editors = $(".summernote-editor");
+       for (var i = 0; i < editors.size(); i++) {
+           var editor = editors[i];
+           var hidden_field = $(editor).next().next();
+           $(hidden_field).val($(editor).code());
+       }
+
+       $(".email-templates").submit();
+   });
 });
