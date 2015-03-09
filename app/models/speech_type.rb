35e5fbec8e20406e8630032b70045135b4705294
Mehmet Beydogan <mbbrain@gmail.com>
2015-03-09 15:28:44 +0200

    Add speech type translating

--- app/models/speech_type.rb
+++ app/models/speech_type.rb
@@ -1,4 +1,6 @@
 class SpeechType < ActiveRecord::Base
+  translates :type_name
+
   belongs_to :conference
   has_many :speeches
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150307201405) do
+ActiveRecord::Schema.define(version: 20150309124410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -211,9 +211,10 @@ ActiveRecord::Schema.define(version: 20150307201405) do
 
   create_table "speech_types", force: true do |t|
     t.integer  "conference_id"
-    t.string   "type_name"
+    t.string   "type_name_en"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "type_name_tr"
   end
 
   create_table "speeches", force: true do |t|
