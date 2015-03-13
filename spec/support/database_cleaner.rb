7add71fba5421feae9c85dfa3e84aac60cd7c8dd
Mehmet Beydogan <mbbrain@gmail.com>
2015-03-13 10:56:05 +0200

    clean up rails_helper file and include support folder update database cleaner config

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,22 @@
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:deletion)
+    Rails.application.load_seed # loading seeds
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :deletion
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
