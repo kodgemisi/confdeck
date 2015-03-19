f84a15207da940a6954981f5167a8efcf93652ac
Mehmet Beydogan <mbbrain@gmail.com>
2015-03-19 12:09:56 +0200

    Fix activity spec

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -2,23 +2,30 @@ require 'rails_helper'
 
 RSpec.describe Activity, :type => :model do
 
-  it 'creates notifications' do
-    @conference = Fabricate(:conference)
-    @admins = Fabricate.times(3, :user)
-    @users = Fabricate.times(3, :user)
-    @conference.conference_admins << @admins
-    @conference.conference_users << @users
+  context 'notifications' do
+    before :each do
+      @conference = Fabricate(:conference)
+      @admins = Fabricate.times(3, :user)
+      @users = Fabricate.times(3, :user)
+      @conference.conference_admins << @admins
+      @conference.conference_users << @users
 
-    activity = Activity.create(
-        user: @admins[1],
-        conference: @conference,
-        action: 'speech_comment',
-        subject: Comment.create
-    )
+    end
+    
+    it 'creates notifications except activitys user' do
+      @activity = Activity.create(
+          user: @admins[0],
+          conference: @conference,
+          action: 'speech_comment',
+          subject: Comment.create
+      )
+      puts User.count
+      expect(Notification.count).to eq(@conference.users.count - 1)
+      expect(@admins[1].notifications.count).to eq(1)
+      expect(@users[1].notifications.count).to eq(1)
+    end
 
-    expect(Notification.count).to eq(@conference.users.count - 1)
-    expect(@admins[1].notifications.count).to eq(1)
-    expect(@users[1].notifications.count).to eq(1)
+
   end
 
 
