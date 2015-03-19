f3f6e6e33752b53db78f7a91ddd5218d70737028
Mehmet Beydogan <mbbrain@gmail.com>
2015-03-19 16:17:23 +0200

    Add notification texts

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,6 +2,6 @@ class NotificationsController < ApplicationController
   before_filter :authenticate_user!
   layout false
   def index
-    @notifications = current_user.notifications
+    @notifications = current_user.notifications.includes(:subject)
   end
 end
--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -5,4 +5,12 @@ module NotificationsHelper
       admin_speech_url(subdomain: notification.subject.subject.commentable.conference.slug, id: notification.subject.subject.commentable_id)
     end
   end
+
+  def notification_text(notification)
+    if notification.comment?
+      comment = notification.subject.subject
+      user = notification.subject.user
+      t('notifications.comment', user: user.display_name, comment: comment.comment, speech: comment.commentable.topic.subject).html_safe
+    end
+  end
 end
